70bb0e21d7d05d863611be8787cacb737b09f702
Emagmunioz <emagmunioz@gmail.com>
2025-02-08 11:19:35 +0100

    PersonaTest run ok

---
+++ src/test/java/dev/eva/PersonaTest.java
@@ -0,0 +1,26 @@
+package dev.eva;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class PersonaTest {
+
+    @Test
+    public void testConstructorYGetters() {
+       
+        Persona persona = new Persona(70.5, 1.75);
+        assertThat(persona.getPeso(), is(70.5));
+        assertThat(persona.getEstatura(), is(1.75));
+    }
+
+    @Test
+    public void testSetters() {
+      
+        Persona persona = new Persona(70.5, 1.75);
+        persona.setPeso(65.0);
+        persona.setEstatura(1.80);
+
+        assertThat(persona.getPeso(), is(65.0));
+        assertThat(persona.getEstatura(), is(1.80));
+    }
+}
