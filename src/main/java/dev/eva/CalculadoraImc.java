bc5eb34d6a2a67d1c2d6736f2061983116036f15
Emagmunioz <emagmunioz@gmail.com>
2025-02-08 11:28:46 +0100

    testCalcularIMC run ok

--- src/main/java/dev/eva/CalculadoraImc.java
+++ src/main/java/dev/eva/CalculadoraImc.java
@@ -1,5 +1,9 @@
 package dev.eva;
 
 public class CalculadoraImc {
+    public static double calcularImc(Persona persona) {
+        return persona.getPeso() / Math.pow(persona.getEstatura(), 2);
+    }
 
 }
+
---
+++ src/test/java/dev/eva/CalculadoraImcTest.java
@@ -0,0 +1,16 @@
+package dev.eva;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class CalculadoraImcTest {
+    @Test
+    public void testCalcularIMC() {
+        Persona persona = new Persona(70, 1.75);
+        double imc = CalculadoraImc.calcularImc(persona);
+        assertThat(imc, closeTo(22.86, 0.01));
+    }
+
+
+}
+
