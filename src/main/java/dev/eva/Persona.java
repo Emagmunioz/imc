624629a2ac1d75883cee7274d59c46aefea64701
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 13:27:26 +0100

    GREEN: Implement Persona constructor and getters to pass tests

--- src/main/java/dev/eva/Persona.java
+++ src/main/java/dev/eva/Persona.java
@@ -1,5 +1,17 @@
 package dev.eva;
 
 public class Persona {
+    private double peso;
+    private double estatura;
+    public Persona(double peso, double estatura) {
+        this.peso = peso;
+        this.estatura = estatura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public double getEstatura() {
+        return estatura;
+    }
 
 }
