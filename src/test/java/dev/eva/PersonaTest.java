249c6af03dd13cab9c37fc1d3589a93cabf693cf
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 13:30:10 +0100

    RED: Add failing test for Persona setters

--- src/test/java/dev/eva/PersonaTest.java
+++ src/test/java/dev/eva/PersonaTest.java
@@ -11,5 +11,13 @@ public class PersonaTest {
         assertThat(persona.getPeso(), is(70.5));
         assertThat(persona.getEstatura (), is(1.75));    
     }
+    @Test
+    public void testSetters() {
+        Persona persona = new Persona(70.5, 1.75);
+        persona.setPeso(80.5);
+        persona.setEstatura(1.80);
+        assertThat(persona.getPeso(), is(80.5));
+        assertThat(persona.getEstatura (), is(1.80));    
+    }
 
 }
