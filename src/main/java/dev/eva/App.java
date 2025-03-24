5c82eee60d337236c33ccb957a3c85f2a82e2d97
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 19:00:02 +0100

    delete everything

--- src/main/java/dev/eva/App.java
+++ src/main/java/dev/eva/App.java
@@ -1,34 +1,17 @@
 package dev.eva;
-import dev.eva.CalculadoraImc;
-import java.util.Scanner;
 
-
+/**
+ * Hello world!
+ */
 public final class App {
     private App() {
     }
 
-        public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Calculadora de IMC");
-        System.out.println("------------------");
-       
-        System.out.print("Ingrese su peso en kg: ");
-        double peso = scanner.nextDouble();
-        
-        System.out.print("Ingrese su estatura en metros: ");
-        double estatura = scanner.nextDouble();
-        
-        
-        Persona persona = new Persona(peso, estatura);
-        double imc = CalculadoraImc.calcularImc(persona);
-        String resultado = CalculadoraImc.obtenerResultadoImc(imc);
-        
-     
-        System.out.printf("\nResultados:%n");
-        System.out.printf("IMC: %.2f%n", imc);
-        System.out.printf("Clasificación: %s%n", resultado);
-        
-        scanner.close();
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
