5340f1df6c6ff550a55fb4387a7e00cf6fa47199
Emagmunioz <emagmunioz@gmail.com>
2025-03-24 13:40:32 +0100

    RED: Add failing test for calcularIMC() - should calculate BMI correctly

--- src/test/java/dev/eva/CalculadoraImcTest.java
+++ src/test/java/dev/eva/CalculadoraImcTest.java
@@ -1,5 +1,16 @@
 package dev.eva;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculadoraImcTest {
+    @Test
+    public void testCalcularImc() {
+        double imc = CalculadoraImc.calcularImc(70.5, 1.75);
+        assertThat(imc, closeTo(22.86, 0.01));
+
+    }
 
 }
